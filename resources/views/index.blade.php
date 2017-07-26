905e5690a1d5db6251572e14a7ff1e0f939699ff
shaozhengxing <shaozhengxingok@126.com>
2017-07-26 15:44:22 +0800

    init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta content="telephone=no" name="format-detection">
+
+        <title>这是标题</title>
+
+        <link rel="icon" href="/favicon.ico" mce_href="/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        <!-- app -->
+        <div id="app"></div>
+        <script src="{{ elixir('js/app.js') }}"></script>
+    </body>
+</html>
